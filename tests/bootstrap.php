1fab59229c862b262740eac7c619d53566a62a66
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-11-15 09:37:26 +0200

    Updated test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,20 +2,6 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:database:drop --force --env=test',
-    __DIR__
-));
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:database:create --no-interaction --env=test',
-    __DIR__
-));
-passthru(sprintf(
-    'php "%s/../bin/console" doctrine:schema:create --no-interaction --env=test',
-    __DIR__
-));
-require __DIR__ . '/../config/bootstrap.php';
-
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
